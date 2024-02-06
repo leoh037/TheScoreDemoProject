243c7b7b85a916ac776a7afc44ec542fcc0b9b1d
Leo <hernleoj@gmail.com>
2024-02-06 02:03:00 -0500

    refactored testclass

--- src/test/java/tests/TheScoreApplicationTests.java
+++ src/test/java/tests/TheScoreApplicationTests.java
@@ -11,16 +11,17 @@ public class TheScoreApplicationTests extends BaseClass {
 	
 	@Test(testName="theScore App Navigation", description="Navigating to the main page of theScore app")
 	public void searchForPlayerTest(){
+		String playerName = "Tajon Buchanan";
 		try {
 			NavigationHelper quickNavigation = new NavigationHelper(driver, test);
 			quickNavigation.applicationQuickSetup();
 			NavigationBar navigation = new NavigationBar(driver, test);
 			navigation.clickOnSearchBar();
 			SearchContentPage searchContentPage = new SearchContentPage(driver, test);
-			searchContentPage.selectPlayer("Tajon Muchanan", "Soccer");
+			searchContentPage.selectPlayer(playerName, "Soccer");
 			SoccerPlayerPage playerPage = new SoccerPlayerPage(driver, test);
-			playerPage.verifySoccerPlayerPage("Tajon Buchanan", "#17 / M / INT");
-			playerPage.verifySoccerPlayerInfo("Tajon Buchanan", "1999-02-08 (Age 24)", "Brampton, Canada", "6'0\"", "150 lbs", "Right");
+			playerPage.verifySoccerPlayerPage(playerName, "#17 / M / INT");
+			playerPage.verifySoccerPlayerInfo(playerName, "1999-02-08 (Age 24)", "Brampton, Canada", "6'0\"", "150 lbs", "Right");
 			playerPage.clickOnBackArrow();
 			searchContentPage.verifySearchContentPage();
 			Thread.sleep(1000);
